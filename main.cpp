b0bd70253f683efb7c90b46dd1622a3d15c8d882
dpro <381941077@qq.com>
2020-07-26 16:05:16 +0800

    third

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<map>
+#include<iostream>
+int main()
+{
+    // std::map<int,char>test;
+    // test.insert(std::pair<int,char>(1,'z'));
+    // test.insert(std::pair<int,char>(2,'t'));
+    // test.erase(3);
+    // std::cout<<test.size();
+    // return 0;
+}
