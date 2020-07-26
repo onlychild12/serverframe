dae1c5c04fae86404a754b4e3e3de106d22a4b61
dpro <381941077@qq.com>
2020-07-26 12:38:16 +0800

    serverframeproject

---
+++ Server.h
@@ -0,0 +1,21 @@
+#ifndef SERVER_H
+#define SERVER_H
+class Server
+{
+private:
+   int server;//监听socket /* data */
+   
+public:
+    Server(/* args */);
+    ~Server();
+};
+
+Server::Server(/* args */)
+{
+}
+
+Server::~Server()
+{
+}
+
+#endif
