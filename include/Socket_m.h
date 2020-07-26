db102b6b31d1978b3b72a07924784c260a52c832
dpro <381941077@qq.com>
2020-07-26 17:18:49 +0800

    third

---
+++ include/Socket_m.h
@@ -0,0 +1,45 @@
+#ifndef SOCKET_M_H
+#define SOCKET_M_H
+#include<sys/socket.h>
+#include<string.h>
+#include<errno.h>
+class Socket_m
+{
+    private:
+    int socket_m;
+    char *data;
+    long package=1024;//包大小
+    Socket_m(int socket_p,int package_p=1024);
+    public:
+   
+    template<class Data>
+    bool litSend(Data *data);//发送
+    template<class Data>
+    void litEncode(char *,Data *);//解码
+    char *rec_m();
+    void setPacksize(long size);
+};
+  template<class Data>
+    bool Socket_m::litSend(Data *data)//发送
+    {
+        char *data_t=new char[package];
+        memset(data_t,'z',package);
+        memcpy(data_t,data,sizeof(data));
+       if(send(socket_m,data_t,package,0)==-1)
+       {
+           return false;
+       }
+       else
+       {
+           return true;/* code */
+       }
+       
+
+    }
+    template<class Data>
+    void Socket_m::litEncode(char*data,Data*desd)
+    {
+            memcpy(desd,data,sizeof(Data));
+            delete[] data;
+    }
+#endif
